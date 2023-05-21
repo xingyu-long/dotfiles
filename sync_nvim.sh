8866f01add862cdadf3920e8567ccd9bdda16e45
Xingyu Long <halolong@hotmail.com>
2023-05-21 12:07:25 -0700

    Rename the folder

--- sync_nvim.sh
+++ sync_nvim.sh
@@ -1,14 +1,14 @@
 
 if [ $1 = "push" ]; then
   # copy config from ~/.config/nvim to . and save as commit
-  cp -r ~/.config/nvim/lua/custom/* ./nvchad_custom_config/custom
+  cp -r ~/.config/nvim/lua/custom/* ./nvchad/custom
   git add .
   git commit
 elif [ $1 == "get" ]; then
   # copy config from git repo to local config dir
   git pull --rebase
   rm -rf ~/.config/nvim/lua/custom
-  cp -r ./nvchad_custom_config/custom ~/.config/nvim/lua
+  cp -r ./nvchad/custom ~/.config/nvim/lua
   cd ~/.config/nvim && nvim .
 else
   echo "wrong argument"
