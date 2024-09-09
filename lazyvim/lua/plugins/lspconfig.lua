5f9c536a969f9ae6a9c0a99ec27924c4a7eaf994
Xingyu Long <halolong@hotmail.com>
2024-09-08 22:54:29 -0700

    Switch to LazyVim

---
+++ lazyvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev stuff
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+
+        -- python
+        --- lsp
+        "pyright",
+        --- formatter
+        "black",
+
+        -- c
+        "clangd",
+      },
+    },
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "javascript",
+        "c",
+        "cpp",
+        "python",
+        "markdown",
+        "markdown_inline",
+      },
+    },
+  },
+}
