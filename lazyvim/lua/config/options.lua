c0cf2d6aa7e24e3588571a27c36a9d9430e08067
Xingyu Long <xingyulong97@gmail.com>
2024-10-05 10:02:29 -0600

    disable autoformat

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,7 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local opt = vim.opt
 
-opt.relativenumber = false -- Relative line numbers
-opt.wrap = true -- wrap the content
+-- disable autoformat
+vim.g.autoformat = false
+
+local opt = vim.opt
+-- Relative line numbers
+opt.relativenumber = false
+-- wrap the content
+opt.wrap = true
